3c98ef83e4407fb73cb8e355ac46b00b14006d70
navitha <navithareddy112@gmail.com>
2023-01-05 09:48:12 +0530

    [navitha] UC1CreateContacts

---
+++ AddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Reflection.Emit;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welome to address book program");
+
+            UC1CreateContacts addressbook = new UC1CreateContacts();
+            addressbook.CreateContact();
+
+
+        }
+    }
+}
---
+++ AddressBook/UC1CreateContacts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class UC1CreateContacts
+    {
+        public void CreateContact()
+        {
+            Console.WriteLine("1.FirstName");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("2.LastName");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("3.Address");
+            string Address = Console.ReadLine();
+
+            Console.WriteLine("4.City");
+            string City = Console.ReadLine();
+
+            Console.WriteLine("5.State");
+            string State = Console.ReadLine();
+
+            Console.WriteLine("6.ZipCode");
+            string ZipCode = Console.ReadLine();
+
+            Console.WriteLine("1.Enter phonenumber");
+            string phonenumber = Console.ReadLine();
+
+
+            Console.WriteLine("1.Enter Email-Id");
+            string Emailid = Console.ReadLine();
+        }
+    }
+}
